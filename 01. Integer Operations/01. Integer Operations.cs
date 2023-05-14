08672f4cc49aab77dc5705a329e773d5b37487ad
tomi45bg <omftomas@gmail.com>
2023-05-14 17:00:00 +0300

    done

---
+++ 01. Integer Operations/01. Integer Operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            int result = 0;
+            int operation1 = num1 + num2;
+            int operation2 = operation1 / num3;
+            int operation3 = operation2 * num4;
+            result = operation3;
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 03. Elevator/03. Elevator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _03._Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((int)Math.Ceiling((double)n / p));
+        }
+    }
+}
